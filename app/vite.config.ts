a7c18377d73069d62da4de2bf6d884825fe1ee16
phuctanpham <phuctanpham@gmail.com>
2025-11-02 15:55:42 +0000

    phucpt: new chart

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
             type: 'image/png',
           },
           {
-            src: 'pwa-512x512.png
+            src: 'pwa-512x512.png',
             sizes: '512x512',
             type: 'image/png',
           },
